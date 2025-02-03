a592aab472a7c4d2b110959a991a332b0e228bdc
Jiri Gebauer <jiri.gebauer.jr@gmail.com>
2025-02-03 14:44:50 +0100

    Add Bucket Instance model and Create Bucket Instance View

---
+++ polystorage/models/bucket_instance.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+class BucketInstance(models.Model):
+    objects = models.Manager()  # Default manager
+    BUCKET_INSTANCE_TYPES = [
+        ('STANDARD', 'Standard Storage Bucket Instance'),
+        ('EXTERNAL', 'External Provider Bucket Instance')
+    ]
+
+    name = models.CharField(max_length=255)
+    root_path = models.CharField(max_length=255)
+    bucket_type = models.CharField(max_length=20, choices=BUCKET_INSTANCE_TYPES, default='STANDARD')
+    external_provider = models.CharField(max_length=255, null=True, blank=True)
+    mount_permissions = models.CharField(max_length=1024, blank=True, default='')
+
+    class Meta:
+        db_table = 'storage_bucket_instance'
+        ordering = ['name']
+
+    def clean(self):
+        if self.bucket_type == 'EXTERNAL':
+            if not self.external_provider:
+                raise ValidationError("External buckets must specify a provider")
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            orig = BucketInstance.objects.get(pk=self.pk)
+            immutable_fields = ['name', 'root_path', 'bucket_type', 'external_provider', 'mount_permissions']
+            for field in immutable_fields:
+                if getattr(self, field) != getattr(orig, field):
+                    raise ValidationError(f"{field} is immutable and cannot be modified")
+
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
--- polystorage/views.py
+++ polystorage/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
 
-# Create your views here.
+from .models.bucket_instance import BucketInstance
+
+
+def create_bucket_instance(request, *args, **kwargs):
+    assert request.method == 'POST'
+    assert "name" in request.POST
+    assert "root_path" in request.POST
+    assert "bucket_type" in request.POST
+    assert "external_provider" in request.POST
+    assert "mount_permissions" in request.POST
+
+    bucket_instance = BucketInstance.objects.create()
+    bucket_instance.name = request.POST["name"]
+    bucket_instance.root_path = request.POST["root_path"]
+    bucket_instance.bucket_type = request.POST["bucket_type"]
+    bucket_instance.external_provider = request.POST["external_provider"]
+    bucket_instance.mount_permissions = request.POST["mount_permissions"]
+    bucket_instance.save()
