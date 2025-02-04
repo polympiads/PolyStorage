74b43c8adad5ff80860a1a8422a2aac9f1a5e20e
Jiri Gebauer <jiri.gebauer.jr@gmail.com>
2025-02-04 14:11:28 +0100

    Add automatic creation of Bucket Instance and signature

--- polystorage/models/bucket.py
+++ polystorage/models/bucket.py
@@ -1,7 +1,10 @@
+import json
+import requests
 from django.db import models
 from django.core.exceptions import ValidationError
 from polystorage.constants import BUCKET_TYPES
-
+from jose import jws
+from django.conf import settings
 
 class Bucket(models.Model):
     objects = models.Manager()  # Default manager
@@ -10,16 +13,17 @@ class Bucket(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True)
     description = models.TextField(blank=True)
+    root_path = models.CharField(max_length=255)
 
     bucket_type = models.CharField(max_length=20, choices=BUCKET_TYPES, default='STANDARD', editable=False)
     external_provider = models.CharField(max_length=255, null=True, blank=True, editable=False)
     cluster = models.CharField(max_length=255, editable=False)  # Cluster is stored as a string
 
-    write_permission_groups = models.CharField(max_length=1024, blank=True, default='')
-    read_permission_groups = models.CharField(max_length=1024, blank=True, default='')
-    delete_permission_groups = models.CharField(max_length=1024, blank=True, default='')
-    mount_permission_groups = models.CharField(max_length=1024, blank=True, default='')
-    observe_permission_groups = models.CharField(max_length=1024, blank=True, default='')
+    write_permissions = models.CharField(max_length=1024, blank=True, default='')
+    read_permissions = models.CharField(max_length=1024, blank=True, default='')
+    delete_permissions = models.CharField(max_length=1024, blank=True, default='')
+    mount_permissions = models.CharField(max_length=1024, blank=True, default='')
+    observe_permissions = models.CharField(max_length=1024, blank=True, default='')
 
     mount_enabled = models.BooleanField(default=False)
     observe_enabled = models.BooleanField(default=False)
@@ -37,7 +41,9 @@ class Bucket(models.Model):
                 raise ValidationError("External buckets must specify a provider")
 
     def save(self, *args, **kwargs):
-        if self.pk:
+        is_new = self.pk is None  # Check if it's a new bucket
+
+        if not is_new:
             orig = Bucket.objects.get(pk=self.pk)
             immutable_fields = ['bucket_type', 'external_provider', 'name', 'created_at', 'cluster']
             for field in immutable_fields:
@@ -45,7 +51,32 @@ class Bucket(models.Model):
                     raise ValidationError(f"{field} is immutable and cannot be modified")
 
         self.clean()
-        super().save(*args, **kwargs)
+        super().save(*args, **kwargs)  # Save first to get the ID
+
+        if is_new:
+            self.create_associated_bucket_instance()
+
+    def create_associated_bucket_instance(self):
+        payload = {
+            'name': self.name,
+            'root_path': self.root_path,
+            'bucket_type': self.bucket_type,
+            'external_provider': self.external_provider,
+            'mount_permissions': self.mount_permissions,
+        }
+
+        # Sign the payload using a private key
+        private_key = settings.STORAGE_ROOT_PRIVATE_KEY
+        signed_payload = jws.sign(payload, private_key, algorithm='RS256')
+
+        # Send the signed payload to the API
+        api_url = settings.API_URL + '/api/v1/bucketinst/create/'
+        headers = {'Content-Type': 'application/json'}
+
+        response = requests.post(api_url, data=json.dumps({"signed_data": signed_payload}), headers=headers)
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to create bucket instance: {response.text}")
 
     def __str__(self):
         return f"{self.name}"
--- polystorage/views.py
+++ polystorage/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from .models.bucket_instance import BucketInstance
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseBadRequest, JsonResponse
+from jose import jws, JWTError
 import json
+from django.conf import settings
 
 @csrf_exempt
 def create_bucket_instance(request, *args, **kwargs):
@@ -12,8 +14,17 @@ def create_bucket_instance(request, *args, **kwargs):
 
     try:
         data = json.loads(request.body.decode("utf-8"))
+        signed_data = data.get("signed_data")
+        if not signed_data:
+            return HttpResponseBadRequest("Missing signed_data")
     except json.JSONDecodeError:
         return HttpResponseBadRequest("Invalid JSON format")
+
+    try:
+        public_key = settings.STORAGE_ROOT_PUBLIC_KEY
+        payload = jws.verify(signed_data, public_key, algorithms=["RS256"])
+    except JWTError:
+        return HttpResponseBadRequest("Invalid signature")
 
     required_fields = ["name", "root_path", "bucket_type", "external_provider", "mount_permissions"]
     missing_fields = [field for field in required_fields if field not in data]
