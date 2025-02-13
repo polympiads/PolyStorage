e8b76dc0333edfc6ba3599ab5b04dce4704eb833
Jiri Gebauer <jiri.gebauer.jr@gmail.com>
2025-02-13 11:07:53 +0100

    Fixes in Access Bucket Instance View

--- polystorage/urls.py
+++ polystorage/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import create_bucket_instance, access_bucket_instance
 
-pattern = ([
-  path('bucketinst/create/', create_bucket_instance)
-], 'bucket', 'bucket')
+bucket_patterns = [
+  path('create/', create_bucket_instance, name='create_bucket_instance'),
+  path('<str:name>/', access_bucket_instance, name='access_bucket_instance'),
+]
 
-urlpatterns = [ path('api/v1/', pattern), path('api/v1/bucketinst/<str:name>/', access_bucket_instance) ]
+urlpatterns = [
+  path('api/v1/bucketinst/', include((bucket_patterns, 'bucket')))
+]
